86892c98b869f61d7ffbc2c8bc3788b157927a2e
Chiptune93 <eoen012@gmail.com>
2023-09-25 02:00:42 +0900

    :construction: 계속 수행될 수 있도록 수정

---
+++ src/main/java/jenkins/springboot/example/HelloController.java
@@ -0,0 +1,13 @@
+package jenkins.springboot.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello! This is test springboot application!";
+    }
+}
