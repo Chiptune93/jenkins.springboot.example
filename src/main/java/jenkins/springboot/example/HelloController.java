fae1ab25bb64e9feed4b49b22e91d4cf30d5da8d
Chiptune93 <eoen012@gmail.com>
2023-09-25 03:39:14 +0900

    :construction: controller change

--- src/main/java/jenkins/springboot/example/HelloController.java
+++ src/main/java/jenkins/springboot/example/HelloController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String hello() {
         return "Hello! This is test springboot application!";
+    }
+
+    @GetMapping("/bye")
+    public String bye() {
+        return "Bye! goodbye from test springboot application!";
     }
 }
